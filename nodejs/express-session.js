bc6329018c1fe3477c874d469fba3bce879f05a7
dy <ekdudd119@naver.com>
2022-03-09 23:56:30 +0900

    0309

--- nodejs/express-session.js
+++ nodejs/express-session.js
@@ -1,19 +1,20 @@
 var express = require('express')
 var parseurl = require('parseurl')
 var session = require('express-session')
-
+  
 var app = express()
-
+  
 app.use(session({
-  secret: 'keyboard cat',
+  secret: 'namanalzihahaha',
   resave: false,
   saveUninitialized: true
 }))
-
+  
 app.get('/', function (req, res, next) {
-  res.send('Hello session');
+    console.log(req.session)
+  res.send('Hello Session!')
 })
-
-app.listen(3000, function() {
-  console.log('3000!')
+ 
+app.listen(3000, function(){
+    console.log('3000!');
 });
